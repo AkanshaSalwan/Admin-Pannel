7eb1832236053f94ffc20d81c19707e4cde10543
Akansha Salwan <akankshasalwan@gmail.com>
2024-09-25 22:11:53 +0530

    admin section update

--- server/models/category.js
+++ server/models/category.js
@@ -1,35 +1,68 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const categoryScheme = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+// const categoryScheme = mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     slug:{
+//         type:String,
+//         required:true,
+//         unique:true
+//     },
+//     images:[
+//         {
+//             type:String,
+//         }
+//     ],
+//     color:{
+//         type:String,
+//     },
+//     parentId:{
+//         type:String,
+//     }
+// },{timestamps:true})
+
+// categoryScheme.virtual('id').get(function(){
+//     return this._id.toHexString();
+// });
+
+// categoryScheme.set('toJSON',{
+//     virtual: true,
+// });
+
+// exports.Category = mongoose.model('Category', categoryScheme);
+// exports.categoryScheme = categoryScheme;
+
+
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
     },
-    slug:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    images:[
+    images: [
         {
-            type:String,
-        }
+            type: String,
+            required: true,
+        },
     ],
-    color:{
-        type:String,
+    color: {
+        type: String,
+        required: true,
     },
-    parentId:{
-        type:String,
-    }
-},{timestamps:true})
+});
 
-categoryScheme.virtual('id').get(function(){
+categorySchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
 
-categoryScheme.set('toJSON',{
-    virtual: true,
+categorySchema.set('toJSON', {
+    virtuals: true,
 });
 
-exports.Category = mongoose.model('Category', categoryScheme);
-exports.categoryScheme = categoryScheme;
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = { Category, categorySchema };
